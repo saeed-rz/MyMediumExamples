5a31a631dc5bfa06034f424e68d242fc7e3aa156
Saeid <saeed.rezaei.sa@gmail.com>
2024-07-21 19:59:55 +0200

    Visualizing Data with Swift Charts: Displaying multiple datasets

--- SwiftCharts.swiftpm/ContentView.swift
+++ SwiftCharts.swiftpm/ContentView.swift
@@ -10,14 +10,30 @@ struct ContentView: View {
         .init(stores: 4, rating: 1),
         .init(stores: 5, rating: 3)
     ]
+    var secondStoresPerfInfo: [PerformanceInfo] = [
+        .init(stores: 0, rating: 2),
+        .init(stores: 1, rating: 3),
+        .init(stores: 2, rating: 1),
+        .init(stores: 3, rating: 4),
+        .init(stores: 4, rating: 5),
+        .init(stores: 5, rating: 4)
+    ]
     var body: some View {
+        let multiplePerformers = [
+            (name: "First Store", info: storesPerfInfo),
+            (name: "Second Store", info: secondStoresPerfInfo),
+        ]
         VStack {
             Text("Stores performance")
-            Chart(storesPerfInfo) { perfInfo in
-                BarMark(
-                    x: .value("Store ID", perfInfo.stores),
-                    y: .value("Rating", perfInfo.rating)
-                )
+            Chart (multiplePerformers, id: \.name) { performer in
+                ForEach(performer.info) { perfInfo in
+                    BarMark(
+                        x: .value("Store ID", perfInfo.stores),
+                        y: .value("Rating", perfInfo.rating)
+                    )
+                    .foregroundStyle(by: .value("Performer", performer.name))
+                    .position(by: .value("Performer", performer.name), axis: .horizontal, span: .inset(15))
+                }
             }
         }
         .padding()
