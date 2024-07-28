91cf26439a6080b4628c45d648cca609a1c5f32a
Saeid <saeed.rezaei.sa@gmail.com>
2024-07-28 19:42:57 +0200

    Surface detection with ARKit

---
+++ Surface Detection/Surface Detection/ContentView.swift
@@ -0,0 +1,49 @@
+//
+//  ContentView.swift
+//  Surface Detection
+
+import SwiftUI
+import RealityKit
+import ARKit
+
+struct ContentView : View {
+    var body: some View {
+        ARViewContainer().edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct ARViewContainer: UIViewRepresentable {
+    
+    func makeUIView(context: Context) -> ARView {
+        
+        let arView = ARView(frame: .zero)
+
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = [.horizontal]
+        configuration.frameSemantics.insert(.personSegmentationWithDepth)
+        arView.session.run(configuration)
+
+        // Create a cube model
+        let mesh = MeshResource.generateBox(size: 0.1, cornerRadius: 0.005)
+        let material = SimpleMaterial(color: .red, roughness: 0.15, isMetallic: true)
+        let model = ModelEntity(mesh: mesh, materials: [material])
+        model.transform.translation.y = 0.05
+
+        // Create horizontal plane anchor for the content
+        let anchor = AnchorEntity(.plane(.horizontal, classification: .any, minimumBounds: SIMD2<Float>(0.2, 0.2)))
+        anchor.children.append(model)
+
+        // Add the horizontal plane anchor to the scene
+        arView.scene.anchors.append(anchor)
+
+        return arView
+        
+    }
+    
+    func updateUIView(_ uiView: ARView, context: Context) {}
+    
+}
+
+#Preview {
+    ContentView()
+}
